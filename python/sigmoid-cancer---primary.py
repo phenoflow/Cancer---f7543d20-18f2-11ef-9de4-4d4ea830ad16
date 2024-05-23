253e87faef8ca9897be918f1f809fbe2f3d9193a
Martin Chapman <contact@martinchapman.co.uk>
2024-05-23 11:58:34 +0100

    Update made by Phenoflow

---
+++ python/sigmoid-cancer---primary.py
@@ -0,0 +1,25 @@
+# Caroline Fairhust, Fabiola Martin, Ian Watt, Tim Doran, Martin Bland, William J Brackenbury, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"B803300","system":"readv2"},{"code":"B133.00","system":"readv2"},{"code":"B14y.00","system":"readv2"},{"code":"B140.00","system":"readv2"},{"code":"B804z00","system":"readv2"},{"code":"B804000","system":"readv2"},{"code":"B14..00","system":"readv2"},{"code":"B804.00","system":"readv2"},{"code":"B14z.00","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('cancer-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["sigmoid-cancer---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["sigmoid-cancer---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["sigmoid-cancer---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
